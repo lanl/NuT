972b4f960e0e7b03d7b5633400ac0367d582aba1
Timothy Kelley <tkelley@lanl.gov>
2015-10-04 07:38:50 -0600

    Update file name.

--- apps/bh-3.cc
+++ apps/bh-3.cc
@@ -1,12 +1,12 @@
-// bh-2.cc
+// bh-3.cc
 // T. M. Kelley
 // June 19, 2012
 // (c) Copyright 2012 LANSLLC, all rights reserved
 
 // C++ version of the bh-2.py program
-// bh-2.cc Differs in that it reads the material state file directly, instead of 
+// bh-2.cc Differs in that it reads the material state file directly, instead of
 // using the MC state file.
-// bh-3.cc further differs in attempting OpenMP & MPI execution, as well as 
+// bh-3.cc further differs in attempting OpenMP & MPI execution, as well as
 // changing to the Philox CBRNG (Salmon et al, SC 2011).
 
 #include "types.hh"
@@ -82,8 +82,8 @@ void run_cycle(src_stat_t const & stats
     using nut::LessThan;
 
     cell_t const n_cells = mesh.n_cells();
-    Require(stats.ns.size()  == n_cells && 
-            stats.es.size()  == n_cells && 
+    Require(stats.ns.size()  == n_cells &&
+            stats.es.size()  == n_cells &&
             stats.ews.size() == n_cells, "correct array lenghts");
     // very large for particles in postprocess--there's really no time
     // CAUTION: this can make sims take a very long time!
@@ -108,10 +108,10 @@ void run_cycle(src_stat_t const & stats
 
     /* This PE does the chunks scheduled by getChunks. Those chunks are listed
      * in chkIds; chkIds are indices into chunks and schunks arrays. For each
-     * chunk, we look at the corresponding vector of source numbers in 
+     * chunk, we look at the corresponding vector of source numbers in
      * schunks[chkId]: for each entry (cell) we run the corresponding number of
      * particles. */
-    
+
 
     for(size_t ci = 0; ci < n_chks_tot; ++ci)
     {
@@ -121,19 +121,19 @@ void run_cycle(src_stat_t const & stats
         src_stat_t const & ss = *schunks[chkId];
         PtclId curr(chunk.pstart);
 
-        // std::cout << "Processing chunk " << chkId 
+        // std::cout << "Processing chunk " << chkId
         //           << ", pstart: " << chunk.pstart
         //           << ", pend: "  << chunk.pend
         //           << std::endl;
 
-        // for each cell in the chunk, process the particles that 
+        // for each cell in the chunk, process the particles that
         // originate in that cell.
         for(uint32_t celli = 0; celli < ss.size(); ++celli)
         {
-            Chnker::sz_t const n_ps = ss.ns[celli]; 
+            Chnker::sz_t const n_ps = ss.ns[celli];
             cell_t const cidx(ss.cidxs[celli]);     // cell idx for this entry
             geom_t const ew(ss.ews[celli]);
-            // std::cout << "*** Processing cell " << celli 
+            // std::cout << "*** Processing cell " << celli
             //           << ", n = " << n_ps << ", cell = " << cidx
             //           << ", ew = " << ew << std::endl;
             for(uint32_t pi = 0; pi < n_ps; ++pi)
@@ -147,10 +147,10 @@ void run_cycle(src_stat_t const & stats
                     mesh,cidx,particle_dt,alpha,s,ew,
                     op.temp(cidx),vel.v(cidx),
                     ptcl_rng);
-                nut::ctr_t evt_ctr(nut::rng_t::make_ctr(0u,0u,ptcl_id,0u)); 
+                nut::ctr_t evt_ctr(nut::rng_t::make_ctr(0u,0u,ptcl_id,0u));
                 nut::rng_t evt_rng(evt_ctr,key);   // for generating events
                 p_in.rng = evt_rng;
-                // p_t p_out = 
+                // p_t p_out =
                 nut::transport_particle(p_in,mesh,op,
                                         vel,tally,census,
                                         log,alpha);
@@ -192,16 +192,16 @@ state_t get_mat_state(std::string const filename,
     }
     vecrows rows(nut::read_mat_state_file<nut::geom_t>(infile));
     infile.close();
-    
-    // get a mesh that includes only those cells within the 
+
+    // get a mesh that includes only those cells within the
     // specified limits; also, get back the indices corresponding
-    // to the limits. 
+    // to the limits.
     size_t llimitIdx(0),ulimitIdx(0);
     Mesh_t mesh = nut::rows_to_mesh<Mesh_t,nut::geom_t>(
         rows, llimit, ulimit, llimitIdx, ulimitIdx);
     Require( ulimitIdx >= llimitIdx,"invalid limits");
     size_t const nrows(ulimitIdx - llimitIdx);
-    Require(mesh.n_cells() == nrows, 
+    Require(mesh.n_cells() == nrows,
             "get_mat_state: mesh size and nrows disagree");
     // get the subset of rows within the limits
     vecrows limitedRows(nrows);
@@ -249,7 +249,7 @@ void run_one_species(nut::Species const spec,
         nut::calc_src_stats_lum(l.nux,cidxs,stats,args.dt,args.n_particles,ncen);
         break;
     default:
-        std::cerr << "run_one_species: unhandled species" 
+        std::cerr << "run_one_species: unhandled species"
                   << nut::species_name(spec) << std::endl;
         throw std::runtime_error("run_one_species: unhandled species");
     }
@@ -263,7 +263,7 @@ void run_one_species(nut::Species const spec,
     uint32_t const commSz(1);
 
     run_cycle( stats,
-               mesh, 
+               mesh,
                op,
                v,
                args.alpha,
@@ -304,7 +304,7 @@ void handler()
         std::cout << stack_syms[i] << "\n";
     }
     free( stack_syms );
-    
+
     exit(1);
 }
 
@@ -312,14 +312,14 @@ void handler()
 int main(int argc, char ** argv)
 {
     // std::set_terminate(handler);
-    
+
     args_t args = parseCL(argc,argv);
 
     state_t state = get_mat_state(args.inputF,args.llimit,args.ulimit);
 
     // for each species, compute source stats, run particles, write tally
     // nu_e
-    
+
     run_one_species(nut::nu_e, args, state);
 
     run_one_species(nut::nu_e_bar, args, state);
