2df05a60a64d68ac0c00cabcba845f5f271871fc
Timothy Kelley <tkelley@lanl.gov>
2015-10-20 16:31:23 -0600

    Migrating x & omega from geom_t (scalar) to vec_t (arb. dimension).

--- lib/Particle.hh
+++ lib/Particle.hh
@@ -7,20 +7,27 @@
 #ifndef PARTICLE_H
 #define PARTICLE_H
 
-#include <ostream>
 #include "types.hh"
+#include "utilities.hh"
+#include "Fates.hh"
+#include <ostream>
+#include <algorithm>
 
 namespace nut
 {
 
-    template <typename fpt, typename rngt>
+    template <typename fpt, typename rngt, size_t Dim = 1>
     struct Particle
     {
+      // types and class constants
+        static const uint32_t dim = Dim;
         typedef fpt  fp_t;
         typedef rngt rng_t;
+        typedef nut::Vec_T<fp_t,dim> vec_t;
 
-        fp_t x;
-        fp_t omega;
+
+        vec_t x;
+        vec_t omega;
         fp_t e;
         fp_t t;
         fp_t weight;
@@ -28,42 +35,48 @@ namespace nut
         rng_t   rng;
         Species species;
         bool alive;
+        Fates fate;
+
 
         // ctor
-        Particle( fp_t x_, 
-                  fp_t omega_,
+        Particle( vec_t x_,
+                  vec_t omega_,
                   fp_t e_,
                   fp_t t_,
                   fp_t weight_,
                   cell_t  cell_,
                   rng_t   rng_,
                   Species s_)
-            : 
-            x    (x_),
-            omega(omega_),
+            :
             e    (e_),
             t    (t_),
             weight (weight_),
             cell   (cell_),
             rng    (rng_),
             species(s_),
-            alive  (true)
-            {}
-            
+            alive  (true),
+            fate(NOT_DEAD_YET)
+            {
+                std::copy(x_.begin(),x_.end(),x.begin() );
+                std::copy(omega_.begin(),omega_.end(), omega.begin() );
+            }
+
+        Particle(){}
+
 
         void kill(){
             alive = false;
         }
 
         bool operator==(Particle const & p) const {
-            bool passed = 
-                x       == p.x &&
+            bool passed =
+                arrays_eq<dim>(x,p.x) &&
                 omega   == p.omega &&
                 e       == p.e &&
                 t       == p.t &&
                 weight  == p.weight &&
                 cell    == p.cell &&
-                rng     == p.rng && 
+                rng     == p.rng &&
                 species == p.species &&
                 alive   == p.alive;
             return passed;
