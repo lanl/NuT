9fbca2f409eb0d0b7fea912c57058cdf3f4c7d2e
Timothy Kelley <tkelley@lanl.gov>
2015-10-20 16:39:31 -0600

    Migrated position & momentum to vectors. Also changed to mesh::sample_direction instead of sampling 1D directly. Also, cleaned up parameterization of apply_nucleon_elastic_scatter.

--- lib/apply_event.hh
+++ lib/apply_event.hh
@@ -20,22 +20,22 @@
 
 namespace nut
 {
-    namespace 
+    namespace
     {
         void underresolved_event(nut::events::Event const & event);
         void unknown_event(nut::events::Event const & event);
     } // anonymous::
 
 
-    template <typename p_t, typename tally_t, 
-              typename vel_t, typename fp_t>
-    void apply_nucleon_elastic_scatter(p_t & p, tally_t & t, 
-                                       vel_t const & vel);
+    template <typename p_t, typename tally_t,
+              typename vel_t, typename Mesh_T>
+    void apply_nucleon_elastic_scatter(p_t & p, tally_t & t, vel_t const & vel,
+                                       Mesh_T const & mesh);
 
     template <typename Mesh_T, typename Particle_T, typename fp_t>
     void stream_particle(Particle_T & p, fp_t const d, Mesh_T const & mesh)
     {
-        typename Mesh_T::coord_t newcoord = 
+        typename Mesh_T::coord_t newcoord =
             mesh.new_coordinate(p.x,p.omega,d);
         p.x     = newcoord.x;
         p.omega = newcoord.omega;
@@ -46,8 +46,8 @@ namespace nut
     template <typename ParticleT, typename MeshT, typename OpacityT,
               typename VelocityT, typename CensusT, typename fp_t>
     void
-    apply_event(ParticleT & p, 
-                events::Event const & event, 
+    apply_event(ParticleT & p,
+                events::Event const & event,
                 geom_t const distance,
                 MeshT const & mesh,
                 OpacityT const & opacity,
@@ -71,15 +71,14 @@ namespace nut
             apply_nucleon_abs(p,tally);
             break;
         case nucleon_elastic_scatter:
-            apply_nucleon_elastic_scatter<ParticleT,Tally<fp_t>,VelocityT,fp_t>(
-                p,tally,velocity);
+            apply_nucleon_elastic_scatter( p,tally,velocity,mesh);
             break;
         case electron_scatter:
             {
                 fp_t const ebar = opacity.m_T.T_e_minus[index];
                 fp_t const e_e  = ebar;
                 // fp_t const e_e  = gen_power_law_energy_alpha2(ebar,p.rng);
-                apply_lepton_scatter(p,tally,e_e,velocity);
+                apply_lepton_scatter(p,tally,e_e,velocity,mesh);
             }
             break;
         case positron_scatter:
@@ -87,14 +86,14 @@ namespace nut
                 fp_t const ebar = opacity.m_T.T_e_plus[index];
                 fp_t const e_e  = ebar;
                 // fp_t const e_e  = gen_power_law_energy_alpha2(ebar,p.rng);
-                apply_lepton_scatter(p,tally,e_e,velocity);
+                apply_lepton_scatter(p,tally,e_e,velocity,mesh);
             }
             break;
         // case nu_e_annhilation:
         //     break;
         // case nu_x_annhilation:
         //     break;
-        case cell_low_x_boundary: 
+        case cell_low_x_boundary:
             apply_low_x_boundary(p,tally);
             break;
         case cell_high_x_boundary:
@@ -110,8 +109,8 @@ namespace nut
             apply_step_end(p,tally,census);
             break;
         // error if we get to an unresolved collision or boundary
-        case boundary:   // fall through to next 
-        case collision: 
+        case boundary:   // fall through to next
+        case collision:
             underresolved_event(event);
             break;
         default:
@@ -137,29 +136,32 @@ namespace nut
     } // apply_nucleon_abs
 
 
-    template <typename p_t, typename tally_t, 
-              typename vel_t, typename fp_t>
-    void apply_nucleon_elastic_scatter(p_t & p, tally_t & t, 
-                                       vel_t const & vel)
+    template <typename p_t, typename tally_t,
+              typename vel_t, typename Mesh_T>
+    void apply_nucleon_elastic_scatter(p_t & p, tally_t & t,
+                                       vel_t const & vel,
+                                       Mesh_T const & msh)
     {
+        // typedef typename tally_t::FP_T fp_t;
+        size_t const dim(tally_t::dim);
         cell_t const cell = p.cell;
         geom_t const v    = vel.v(cell);
         geom_t const eli  = p.e;
-        geom_t const oli  = p.omega;
+        vec_t<dim> const oli  = p.omega;
         // LT to comoving frame (need init comoving e to compute
         // final lab e).
-        EandOmega eno_cmi = LT_to_comoving_sphere1D(v,eli,oli);
+        EandOmega<dim> eno_cmi = LT_to_comoving_sphere1D(v,eli,oli);
         geom_t const eci  = eno_cmi.first;
-        // scatter
-        fp_t const seed  = p.rng.random();
-        geom_t const ocf = 2 * seed - 1;
+        // scatter: sample a new direction
+        vec_t<dim> ocf = msh.sample_direction(p.rng);
+
         // LT comoving -> lab
-        EandOmega const eno_lf = LT_to_lab_sphere1D(v,eci,ocf);
+        EandOmega<dim> const eno_lf = LT_to_lab_sphere1D(v,eci,ocf);
         geom_t const elf = eno_lf.first;
-        geom_t const olf = eno_lf.second;
+        vec_t<dim> const olf = eno_lf.second;
         // tally
         t.count_nucleon_elastic_scatter(p.cell);
-        t.deposit_inelastic_scat(cell, eli, elf, oli, olf, 
+        t.deposit_inelastic_scat(cell, eli, elf, oli, olf,
                                  p.weight, p.species);
         // update particle
         p.omega = olf;
@@ -169,28 +171,29 @@ namespace nut
     }
 
     template <typename p_t, typename tally_t,
-              typename velocity_t, typename fp_t>
-    void apply_lepton_scatter(p_t & p, tally_t & t, fp_t const e_lep, 
-                              velocity_t const & vel)
+              typename velocity_t, typename fp_t, typename Mesh_T>
+    void apply_lepton_scatter(p_t & p, tally_t & t, fp_t const e_lep,
+                              velocity_t const & vel,
+                              Mesh_T const & msh)
     {
+        uint32_t const dim(p_t::dim);
         cell_t const cell = p.cell;
         geom_t const v    = vel.v(cell);
         geom_t const eli  = p.e;
-        geom_t const oli  = p.omega;
+        vec_t<dim> const oli  = p.omega;
 
         // LT to comoving frame (need init comoving e to compute
         // final lab e).
-        EandOmega eno_cmi = LT_to_comoving_sphere1D(v,eli,oli);
+        EandOmega<dim> eno_cmi = LT_to_comoving_sphere1D(v,eli,oli);
         geom_t const eci  = eno_cmi.first;
         // scatter
-        fp_t seed = p.rng.random();
-        fp_t ocf = 2 * seed - 1;
+        vec_t<dim> ocf = msh.sample_direction(p.rng);
         fp_t const de = (eci - e_lep)/4.;
         fp_t const ecf = eci - de;
         // LT comoving -> lab
-        EandOmega const eno_lf = LT_to_lab_sphere1D(v,ecf,ocf);
+        EandOmega<dim> const eno_lf = LT_to_lab_sphere1D(v,ecf,ocf);
         geom_t const elf = eno_lf.first;
-        geom_t const olf = eno_lf.second;
+        vec_t<dim> const olf = eno_lf.second;
         // tally
         t.count_lepton_scatter(p.cell,p.species);
         t.deposit_inelastic_scat(cell,eli,elf,oli,olf,p.weight,
@@ -201,7 +204,7 @@ namespace nut
         return;
     } // apply_lepton_scatter
 
-   
+
     template <typename p_t, typename tally_t>
     void apply_low_x_boundary(p_t & p, tally_t & t)
     {
@@ -243,16 +246,16 @@ namespace nut
         return;
     }
 
-    namespace 
+    namespace
     {
         void underresolved_event(nut::events::Event const & event)
         {
             std::stringstream errstr;
-            errstr << "Event not fully resolved: event type " 
+            errstr << "Event not fully resolved: event type "
                    << nut::events::event_name(event);
             throw std::runtime_error(errstr.str());
         }
-        
+
         void unknown_event(nut::events::Event const & event)
         {
             std::stringstream errstr;
@@ -272,6 +275,6 @@ namespace nut
 // End of file
 
 
-/* 
-"collision","nucleon_abs","nucleon_elastic_scatter","electron_scatter","positron_scatter","nu_e_annhilation","nu_x_annhilation","boundary","cell_low_x_boundary","cell_high_x_boundary","escape","reflect","step_end" 
+/*
+"collision","nucleon_abs","nucleon_elastic_scatter","electron_scatter","positron_scatter","nu_e_annhilation","nu_x_annhilation","boundary","cell_low_x_boundary","cell_high_x_boundary","escape","reflect","step_end"
 */
