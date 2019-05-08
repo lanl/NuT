1a8cc810146099f8fbce033db4627ee1f924125a
Timothy Kelley <tkelley@lanl.gov>
2019-05-08 15:28:07 -0600

    Idea is to transition to a new mesh based on murmeln Mesh_Interface.

---
+++ lib/Mesh_3D_Cartesian.hh
@@ -0,0 +1,15 @@
+// Mesh_3D_Cartesian.hh
+// T. M. Kelley
+// May 07, 2019
+// (c) Copyright 2019 Triad National Security, all rights reserved
+
+
+#pragma once
+
+#include "murmeln/mesh_adaptors/Cartesian_Mesh_Interface.h"
+
+namespace nut{
+
+} // nut::
+
+// End of file
