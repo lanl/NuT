02bea65d549229cc566d7d1856c6fdb80fef71d5
Timothy Kelley <tkelley@lanl.gov>
2020-06-25 08:27:44 -0600

    Ugh, integer types.

--- lib/meshes/mesh_common/types.h
+++ lib/meshes/mesh_common/types.h
@@ -13,7 +13,7 @@ using index_t = ::uint64_t;
 
 index_t constexpr max_index_t = std::numeric_limits<index_t>::max();
 
-using cell_index_t = uint32_t;
+using cell_index_t = index_t;
 
 using geom_t = double;
 
