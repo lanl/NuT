378402d95e3e7110173f7c43967c75c4e5737b87
Timothy Kelley <tkelley@lanl.gov>
2020-06-25 12:36:55 -0600

    add some explanation

--- docker/test_nut.sh
+++ docker/test_nut.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
+ echo "branch is "${branch}
  branch=$1
  git clone https://github.com/lanl/nut.git -b $branch nut
  cd nut
+ git log -n 1 --oneline
+ git branch
  mkdir build
  cd build
  cmake ..
